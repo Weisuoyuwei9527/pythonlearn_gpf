099e4cb21d012c0378182c16af10b0ff956ceca4
Weisuoyuwei9527 <39695446+Weisuoyuwei9527@users.noreply.github.com>
2018-12-26 17:24:21 +0800

    This is a tensorflow demo with python.

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,6))
+X_train = np.linspace(0.1,0.5,100)
+Y_train = 8*X_train+5+np.random.randn(100)*0.123
+plt.plot(X_train,Y_train,'go',label='raw data')
+plt.legend()
+plt.show()
