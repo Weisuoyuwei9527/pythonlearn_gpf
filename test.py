1f0a586e6aef8841fa58154e4801a8d1578940c4
Weisuoyuwei9527 <phenixfk@163.com>
2019-01-08 17:31:30 +0800

    This is a tensorflow demo with python111.

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ plt.figure(figsize=(10,6))
 X_train = np.linspace(0.1,0.5,100)
 Y_train = 8*X_train+5+np.random.randn(100)*0.123
 plt.plot(X_train,Y_train,'go',label='raw data')
-print("测试！")
+print("测试以下！")
 plt.legend()
 plt.show()
